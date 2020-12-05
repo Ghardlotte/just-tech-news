d51ce5029d33b5331a847663f77617a68bb54e3b
Ghardlotte <g_hardlotte@hotmail.com>
2020-12-05 16:16:46 -0600

    added password check

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -66,6 +66,13 @@ router.post('/', (req, res) => {
     
         // Verify user
         const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+          }
+          
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+
       });  
     });
 
